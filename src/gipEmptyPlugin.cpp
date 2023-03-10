e6a7b3c7798c364d10a6458f58ea001dc4e87b14
R.Noyan Culum <noyanculum@yahoo.com>
2023-03-11 01:55:36 +0300

    Plugin repo renewed

---
+++ src/gipEmptyPlugin.cpp
@@ -0,0 +1,17 @@
+/*
+ * gipEmptyPlugin.cpp
+ *
+ *  Created on: Mar 11, 2023
+ *      Author: Noyan Culum
+ *      Created by: kayra
+ */
+
+#include "gipEmptyPlugin.h"
+
+
+gipEmptyPlugin::gipEmptyPlugin() {
+}
+
+gipEmptyPlugin::~gipEmptyPlugin() {
+}
+
